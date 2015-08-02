87b591ef59bb93bf03d22f266c358ed09182cfe0
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-02 21:31:27 +0200

    provision libuv -- server to host web applications

--- provision.sh
+++ provision.sh
@@ -6,10 +6,20 @@
 
 # Get a working version of Mono
 apt-key adv --keyserver pgp.mit.edu --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
 apt-get -y update
 apt-get -y install mono-complete
 
 # Get and compile libuv (Required for the Kestrel server)
+apt-get -y install automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sh autogen.sh
+./configure
+make 
+make install
+rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+ldconfig
+
 # Get DNVM
 # Add sources to NuGet.config (For package restore)
