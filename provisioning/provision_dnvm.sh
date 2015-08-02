12d94458fb1fd7019daab945e7cbbf41549d1165
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-02 22:09:38 +0200

    fixed faulty command

--- provisioning/provision_dnvm.sh
+++ provisioning/provision_dnvm.sh
@@ -3,6 +3,3 @@
 # Get DNVM
 apt-get -y install unzip
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
-
-# Add sources to NuGet.config (For package restore)
-config.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
