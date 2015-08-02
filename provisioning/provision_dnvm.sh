77f36555b7a6031020d869acc14b632a2f5dbb29
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-02 22:31:15 +0200

    provision a dnx

--- provisioning/provision_dnvm.sh
+++ provisioning/provision_dnvm.sh
@@ -2,4 +2,8 @@
 
 # Get DNVM
 apt-get -y install unzip
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh
+
+source ~/.dnx/dnvm/dnvm.sh
+
+dnvm upgrade
